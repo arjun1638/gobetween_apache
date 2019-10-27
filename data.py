835a9648078fb0f7267f84b026c6bcd7fda6b5ec
arjun1638 <arjun1638@gmail.com>
2019-10-27 16:16:30 +0100

    data collection through api for arjun1638/gob_tng

--- data.py
+++ data.py
@@ -1,5 +1,11 @@
 import requests
 import json
 r=requests.get(url='http://20.0.0.254:8888/servers/sample/stats')
-with open('/tngbench_share/gob-servers-stat-data.txt','w') as f:
-    json.dump(r.json(),f)
+data={}
+data["stats"]=r.json()
+r=requests.get(url='http://20.0.0.254:8888/servers/')
+data["servers"]=r.json()
+r=requests.get(url='http://20.0.0.254:8888/servers/sample')
+data["server_arrangements"]=r.json()
+with open('/tngbench_share/data.txt','w') as f:
+    json.dump(data,f)

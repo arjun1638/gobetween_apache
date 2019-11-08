f513a509ebb4817f0defdb70dee17f922b487890
arjun1638 <arjun1638@gmail.com>
2019-11-09 00:03:02 +0100

    json fix

--- data.py
+++ data.py
@@ -3,10 +3,10 @@ import json
 import csv
 r=requests.get(url='http://20.0.0.254:8888/servers/sample/stats')
 data={}
-data["stats"]=json.load(r.json())
+data["stats"]=r.json()
 r=requests.get(url='http://20.0.0.254:8888/servers/')
-data["servers"]=json.load(r.json())
+data["servers"]=r.json()
 r=requests.get(url='http://20.0.0.254:8888/servers/sample')
-data["server_arrangements"]=json.load(r.json())
+data["server_arrangements"]=r.json()
 with open('/tngbench_share/go_data.txt','w') as f:
     json.dump(data,f)

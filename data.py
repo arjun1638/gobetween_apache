68ae4dc20fd5b4a6f21eae13b7def9ee99bb0487
arjun1638 <arjun1638@gmail.com>
2019-11-08 21:26:31 +0100

    data collection script update

--- data.py
+++ data.py
@@ -1,5 +1,6 @@
 import requests
 import json
+import csv
 r=requests.get(url='http://20.0.0.254:8888/servers/sample/stats')
 data={}
 data["stats"]=r.json()
@@ -9,3 +10,11 @@ r=requests.get(url='http://20.0.0.254:8888/servers/sample')
 data["server_arrangements"]=r.json()
 with open('/tngbench_share/data.txt','w') as f:
     json.dump(data,f)
+with open('/tngbench_share/go_data.csv', 'w') as f:
+    for key in data.keys():
+        f.write("%s,%s\n"%(key,data[key]))
+
+with open('/tngbench_share/go_data.csv', 'w') as f:  # Just use 'w' mode in 3.x
+    w = csv.DictWriter(f, data.keys())
+    w.writeheader()
+    w.writerow(data)
--- stop.sh
+++ stop.sh
@@ -1,3 +1,10 @@
 python ./data.py 
 
+sleep 2
+python ./log_intf_statistics.py /tngbench_share/result.yml
+
+sleep 2
+python ./process_ab_results.py /tngbench_share/cmd_start.log /tngbench_share/go_data.csv /tngbench_share/result.yml
+
+
 date > /mnt/share/stop.txt

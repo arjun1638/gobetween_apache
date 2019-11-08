6e902278a690d044d607911d533f705c40c58835
arjun1638 <arjun1638@gmail.com>
2019-11-08 22:41:55 +0100

    data conversion

--- data.py
+++ data.py
@@ -3,11 +3,11 @@ import json
 import csv
 r=requests.get(url='http://20.0.0.254:8888/servers/sample/stats')
 data={}
-data["stats"]=r.json()
+data["stats"]=json.loads(r.json())
 r=requests.get(url='http://20.0.0.254:8888/servers/')
-data["servers"]=r.json()
+data["servers"]=json.loads(r.json())
 r=requests.get(url='http://20.0.0.254:8888/servers/sample')
-data["server_arrangements"]=r.json()
+data["server_arrangements"]=json.loads(r.json())
 with open('/tngbench_share/data.txt','w') as f:
     json.dump(data,f)
 with open('/tngbench_share/go_data.csv', 'w') as f:

881a9263226cbb297fd8d20509ec66356dbd0019
arjun1638 <arjun1638@gmail.com>
2019-11-11 23:50:13 +0100

    file format update

--- data.py
+++ data.py
@@ -4,9 +4,9 @@ import csv
 r=requests.get(url='http://20.0.0.254:8888/servers/sample/stats')
 data={}
 data["stats"]=r.json()
-r=requests.get(url='http://20.0.0.254:8888/servers/')
-data["servers"]=r.json()
-r=requests.get(url='http://20.0.0.254:8888/servers/sample')
-data["server_arrangements"]=r.json()
-with open('/tngbench_share/go_data.txt','w') as f:
-    json.dump(data,f)
+#r=requests.get(url='http://20.0.0.254:8888/servers/')
+#data["servers"]=r.json()
+#r=requests.get(url='http://20.0.0.254:8888/servers/sample')
+#data["server_arrangements"]=r.json()
+with open('/tngbench_share/go_data.json','w') as f:
+    json.dump(data,f,separators=(',',':'),indent=None)
